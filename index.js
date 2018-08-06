54b2a47b23b90af03d810a3ff1a2e42bccf78ff3
Jonathan Stricklin <jstricklin89@gmail.com>
2018-08-06 17:07:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// add solution here
+function theBeatlesPlay(musicianArray, instrumentArray) {
+  var emptyArray = [] 
+    for (var i = 0; i < musicianArray.length; i++) {
+    emptyArray.push(musicianArray[i] + " plays " + instrumentArray[i])
+    }
+    return emptyArray
+}
+function johnLennonFacts(facts) {
+  var newFacts = []
+  var i = 0
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!")
+    i++
+  }
+  return newFacts
+}
+function iLoveTheBeatles(number) {
+ var = []
+ 
+}
