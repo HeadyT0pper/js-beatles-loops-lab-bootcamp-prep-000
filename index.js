ca69ec8a1d70540e8b725a4db187b8ed935873a5
Jonathan Stricklin <jstricklin89@gmail.com>
2018-08-06 17:17:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(n) {
  var love = []
  do {
-   love.push(n + "I love the Beatles!")
+   love.push("I love the Beatles!")
    n++
  } while (n < 15)
  return love
